1bc59e397794cc514a27a4423e8a7d4757b864ec
Krish Mehta <123krishmehta@gmail.com>
2026-07-03 20:49:25 +0530

    Update pwm_test.c

--- Task-6/Resources/Firmware/pwm_test.c
+++ Task-6/Resources/Firmware/pwm_test.c
@@ -1,1 +1,32 @@
+#include <stdint.h>
 
+
+#define PWM_BASE    0x40000000
+
+#define PWM_CTRL    (*(volatile uint32_t *)(PWM_BASE + 0x00))
+#define PWM_PERIOD  (*(volatile uint32_t *)(PWM_BASE + 0x04))
+#define PWM_DUTY    (*(volatile uint32_t *)(PWM_BASE + 0x08))
+#define PWM_STATUS  (*(volatile uint32_t *)(PWM_BASE + 0x0C))
+
+
+void main()
+{
+
+    // Set PWM period
+    PWM_PERIOD = 100;
+
+
+    // 50% Duty Cycle
+    PWM_DUTY = 50;
+
+
+    // Enable PWM
+    PWM_CTRL = 1;
+
+
+    while(1)
+    {
+
+    }
+
+}
