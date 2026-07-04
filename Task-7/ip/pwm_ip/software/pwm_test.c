da0cf8f643b2ee0e6ab6b31b15d18092d44b41d7
Krish Mehta <123krishmehta@gmail.com>
2026-07-04 17:50:58 +0530

    Create pwm_test.c

---
+++ Task-7/ip/pwm_ip/software/pwm_test.c
@@ -0,0 +1,67 @@
+#include <stdint.h>
+
+
+#define PWM_BASE 0x40000000
+
+
+#define PWM_CTRL    (*(volatile uint32_t *)(PWM_BASE + 0x00))
+#define PWM_PERIOD  (*(volatile uint32_t *)(PWM_BASE + 0x04))
+#define PWM_DUTY    (*(volatile uint32_t *)(PWM_BASE + 0x08))
+#define PWM_STATUS  (*(volatile uint32_t *)(PWM_BASE + 0x0C))
+
+
+
+int main()
+{
+
+    /*
+        Configure PWM Period
+
+        This value controls the total PWM cycle length.
+    */
+
+    PWM_PERIOD = 10;
+
+
+
+    /*
+        Configure Duty Cycle
+
+        Duty Cycle = DUTY / PERIOD × 100
+
+        Here:
+
+        Duty = 5
+        Period = 10
+
+        Duty Cycle = 50%
+    */
+
+    PWM_DUTY = 5;
+
+
+
+    /*
+        Enable PWM IP
+
+        CTRL[0] = 1 enables PWM output
+    */
+
+    PWM_CTRL = 1;
+
+
+
+    while(1)
+    {
+
+        /*
+            PWM hardware continues generating
+            output waveform independently
+        */
+
+    }
+
+
+    return 0;
+
+}
